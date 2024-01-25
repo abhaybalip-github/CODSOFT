ccea371613ce7e5960f3b59c630df6237d636b7a
Abhay Balip <abhay.balip@gmail.com>
2024-01-25 14:48:32 +0530

    Note Added

--- cpp_Programs/ToDoList.cpp
+++ cpp_Programs/ToDoList.cpp
@@ -6,26 +6,53 @@
 #include <map>
 #include <iterator>
 
-// map data structure for storing list
-std::map<int, std::string> Task_List;
-// list handeling function
-void Add_Item(std::string task)
+std::vector<std::string> task_list;
+std::vector<bool> task_status;
+bool close = false;
+void insert_task(std::string task)
 {
-    Task_List.insert({Task_List.size() + 1, task});
+    task_list.push_back(task);
+    task_status.push_back(1);
+    std::cout << "Task Added \n\n";
 }
-void Remove_Item(int key)
+void delete_task(int key)
 {
-    Task_List.erase(key);
-}
-void Display_List()
-{
-    for (std::iterator<int, std::string> itr = Task_List.begin(); itr < Task_List.end(); itr++)
+    if (task_list.size() > 0)
     {
+        task_list.erase(task_list.begin() + key - 1);
+        std::cout << "Task Removed \n\n";
+    }
+    else
+    {
+        std::cout << "No Task Avaliable \n\n";
+    }
+}
+void display_task()
+{
+    if (task_list.size() > 0)
+    {
+        std::cout << "No.\tStatus\tTask\n";
+        for (int i = 0; i < task_list.size(); i++)
+        {
+            /* code */
+            std::cout << i + 1 << "\t";
+            task_status[i] ? std::cout << "Active  \t" : std::cout << "InActive\t";
+            std::cout << task_list[i] << " \n";
+        }
+    }
+    else
+    {
+        std::cout << "No Task Avaliable \n\n";
     }
 }
 int main(int argc, char const *argv[])
 {
     /* code */
+    while (!close)
+    {
+        /* code */
+        if ()
+    }
 
     return 0;
 }
