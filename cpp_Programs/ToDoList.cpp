ef2ee9203dbd33eed129d879178fee814edfa816
Abhay Balip <abhay.balip@gmail.com>
2024-01-25 23:07:46 +0530

    code updated

--- cpp_Programs/ToDoList.cpp
+++ cpp_Programs/ToDoList.cpp
@@ -1,25 +1,30 @@
 // cpp program
 // console based to-do list
 #include <iostream>
-#include <math.h>
+#include <stdio.h>
 #include <vector>
-#include <map>
 #include <iterator>
 
 std::vector<std::string> task_list;
 std::vector<bool> task_status;
 bool close = false;
+int choice;
+
+// function to insert new task in list
 void insert_task(std::string task)
 {
     task_list.push_back(task);
     task_status.push_back(1);
     std::cout << "Task Added \n\n";
 }
+
+// delete a task from list
 void delete_task(int key)
 {
     if (task_list.size() > 0)
     {
         task_list.erase(task_list.begin() + key - 1);
+        task_status.erase(task_status.begin() + key - 1);
         std::cout << "Task Removed \n\n";
     }
     else
@@ -27,6 +32,21 @@ void delete_task(int key)
         std::cout << "No Task Avaliable \n\n";
     }
 }
+
+// reverse the task status
+void change_status(int key)
+{
+    if (task_list.size() <= key)
+    {
+        task_status[key - 1] = !task_status[key - 1];
+    }
+    else
+    {
+        std::cout << "No Task Avaliable \n\n";
+    }
+}
+
+// display task list
 void display_task()
 {
     if (task_list.size() > 0)
@@ -45,13 +65,59 @@ void display_task()
         std::cout << "No Task Avaliable \n\n";
     }
 }
+
+// 
+void store_task_list()
+{
+}
 int main(int argc, char const *argv[])
 {
     /* code */
     while (!close)
     {
         /* code */
-        if ()
+        std::cout << "\t* Welcome to Task List *\n\n"
+                  << "Avaliable Options :: \n";
+        std::cout << "1.Enter Task\t2.Delete Task\n3.Display Task\t4.Change Status\n";
+        std::cout << "5.Close Task\t6.Store in File\n\n";
+
+        switch (choice)
+        {
+        case 1:
+        {
+            std::string task;
+            std::cout << "Enter task: ";
+            std::getline(std::cin, task);
+            insert_task(task);
+            break;
+        }
+        case 2:
+        {
+            int task_num;
+            std::cout << "Enter task number to delete: ";
+            std::cin >> task_num;
+            delete_task(task_num);
+            break;
+        }
+        case 3:
+            display_task();
+            break;
+        case 4:
+        {
+            int task_num;
+            std::cout << "Enter task number to change status: ";
+            std::cin >> task_num;
+            change_status(task_num);
+            break;
+        }
+        case 5:
+            std::cout << "Closing task list...\n";
+        case 6:
+            store_task_list();
+            break;
+        default:
+            std::cout << "Invalid choice!\n\n";
+        }
     }
 
     return 0;
